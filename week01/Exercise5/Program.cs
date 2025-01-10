a29c52312851f9fb4ea2cb524036c5787d3b2bd1
laisadubiela <laisa.sf@hotmail.com>
2025-01-09 21:27:34 -0300

    exercise 5

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -5,5 +5,30 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumberSquared = PromptUserNumber();
+        Console.WriteLine($"{userName}, your favorite number squared is {userNumberSquared}");
+    }    
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine($"Welcome to the Program.");
+    }
+
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name:");
+        string name = Console.ReadLine();
+        return name;    
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter your favorite number:");
+        string userResponse = Console.ReadLine();
+        int userNumber = int.Parse(userResponse);
+        int SquareNumber = userNumber * userNumber;
+        return SquareNumber;
     }
 }
