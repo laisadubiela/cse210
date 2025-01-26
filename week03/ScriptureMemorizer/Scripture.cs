86589e0f417985f753a30ef1034e71cbe24e1064
laisadubiela <laisa.sf@hotmail.com>
2025-01-25 22:02:25 -0300

    design estructure

---
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+
+public class Scripture
+{
+    private Reference _reference;
+    private readonly List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        
+    }
+
+    public string GetDisplayText()
+    {
+        
+    }
+
+    public bool IsCompleteHidden()
+    {
+        
+    }
+}
